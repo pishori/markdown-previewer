ade3227bd5a83fb3c062f632165e6e7998f17ab0
Alvin <70914856+pishori@users.noreply.github.com>
2024-01-27 13:51:09 +0300

    added Maradona image

--- src/App.tsx
+++ src/App.tsx
@@ -28,13 +28,6 @@ And feel free to go crazy ~~crossing stuff out~~.
 There's also [links](https://www.freecodecamp.org), and
 > Block Quotes!
 
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | -------------
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
-
 - And of course there are lists.
   - Some are bulleted.
      - With different indentation levels.
@@ -45,7 +38,7 @@ And here. | Okay. | I think we get it.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![React Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+![Maradona image](https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Maradona-Mundial_86_con_la_copa.JPG/330px-Maradona-Mundial_86_con_la_copa.JPG)
 
 `
 
